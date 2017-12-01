612cd1470c192e90fdd717bc7a095d71b6483181
leonardo <ljsea6@gmail.com>
2017-12-01 13:06:06 -0500

    inscripcion en ambas tiendas lista

--- resources/views/admin/send/welcome.blade.php
+++ resources/views/admin/send/welcome.blade.php
@@ -340,21 +340,7 @@ a {
 </h1>
                                 </td>
                             </tr>
-                            <tr>
-                                <td style="padding: 0 40px 40px; font-family: sans-serif; font-size: 15px; line-height: 140%; color: #555555;">
-                                    <!-- Button : BEGIN -->
-                                    <table role="presentation" cellspacing="0" cellpadding="0" border="0" align="center" style="margin: auto;">
-                                        <tr>
-                                            <td style="border-radius: 3px;  text-align: center;" class="button-td">
-                                                <a href="http://demo.tiendagood.com/?q={{$code}}" style="background: #222222; border: 15px solid #222222; font-family: sans-serif; font-size: 13px; line-height: 110%; text-align: center; text-decoration: none; display: block; border-radius: 3px; font-weight: bold;" class="button-a">
-                                                    <span style="color:#ffffff;" class="button-link"></span>
-                                                </a>
-                                            </td>
-                                        </tr>
-                                    </table>
-                                    <!-- Button : END -->
-                                </td>
-                            </tr>
+
                         </table>
                     </td>
                 </tr>
