7642f2bcd8f118a1661d9781d1d61dc63379feaf
leonardo <ljsea6@gmail.com>
2017-12-11 19:59:54 -0500

    cambio en vista para mostrar puntos propios y de los niveles

--- app/Traits/OrderPaid.php
+++ app/Traits/OrderPaid.php
@@ -6,11 +6,13 @@
  * Time: 10:35 AM
  */
 namespace App\Traits;
+
 use DB;
 use App\Order;
 use App\Variant;
 use Carbon\Carbon;
 use App\Entities\Tercero;
+
 trait OrderPaid
 {
     public function OrderPaid($order, $order_create, $puntos)
@@ -23,7 +25,6 @@ trait OrderPaid
                         ->where('shop', 'good')
                         ->first();
                     if (count($variant) > 0) {
-
                         DB::table('variants')
                             ->where('id', $item['variant_id'])
                             ->where('product_id', $item['product_id'])
@@ -32,9 +33,11 @@ trait OrderPaid
                     }
                 }
             }
+
             $tercero = Tercero::where('email', strtolower($order['email']))
                 ->where('state', true)
                 ->first();
+
             if (count($tercero) > 0) {
 
                 $update = Tercero::with('networks', 'levels', 'cliente')->find($tercero->id);
@@ -147,13 +150,20 @@ trait OrderPaid
                                 }
                             }
                         }
+
                     } else {
+
                         $update->mispuntos = $update->mispuntos + $order_create->points;
                         $update->save();
+
                         if (count($update->networks) > 0) {
+
                             $padre_uno = Tercero::with('networks', 'levels')->find($update->networks[0]['pivot']['padre_id']);
+
                             if (count($padre_uno) > 0 && $padre_uno->state == true) {
+
                                 if (count($padre_uno->levels) == 0) {
+
                                     DB::table('terceros_niveles')->insertGetId(
                                         [
                                             'tercero_id' => $padre_uno->id,
@@ -166,12 +176,15 @@ trait OrderPaid
                                         ->where('tercero_id', $padre_uno->id)
                                         ->where('nivel', 1)
                                         ->first();
+
                                     if (count($result) > 0) {
+
                                         DB::table('terceros_niveles')
                                             ->where('tercero_id', $padre_uno->id)
                                             ->where('nivel', 1)
                                             ->update(['puntos' => $result->puntos + $puntos]);
                                     } else {
+
                                         DB::table('terceros_niveles')->insertGetId(
                                             [
                                                 'tercero_id' => $padre_uno->id,
@@ -181,10 +194,15 @@ trait OrderPaid
                                         );
                                     }
                                 }
+
                                 if (count($padre_uno->networks) > 0) {
+
                                     $padre_dos = Tercero::with('networks', 'levels')->find($padre_uno->networks[0]['pivot']['padre_id']);
+
                                     if (count($padre_dos) > 0 && $padre_dos->state == true) {
+
                                         if (count($padre_dos->levels) == 0) {
+
                                             DB::table('terceros_niveles')->insertGetId(
                                                 [
                                                     'tercero_id' => $padre_dos->id,
@@ -192,11 +210,14 @@ trait OrderPaid
                                                     'puntos' =>  $puntos,
                                                 ]
                                             );
+
                                         } else {
+
                                             $result = DB::table('terceros_niveles')
                                                 ->where('tercero_id', $padre_dos->id)
                                                 ->where('nivel', 2)
                                                 ->first();
+
                                             if (count($result) > 0) {
                                                 DB::table('terceros_niveles')
                                                     ->where('tercero_id', $padre_dos->id)
@@ -212,10 +233,15 @@ trait OrderPaid
                                                 );
                                             }
                                         }
+
                                         if (count($padre_dos->networks) > 0) {
+
                                             $padre_tres = Tercero::with('networks', 'levels')->find($padre_dos->networks[0]['pivot']['padre_id']);
+
                                             if (count($padre_tres) > 0 && $padre_tres->state == true) {
+
                                                 if (count($padre_tres->levels) == 0) {
+
                                                     DB::table('terceros_niveles')->insertGetId(
                                                         [
                                                             'tercero_id' => $padre_tres->id,
@@ -224,15 +250,18 @@ trait OrderPaid
                                                         ]
                                                     );
                                                 } else {
+
                                                     $result = DB::table('terceros_niveles')
                                                         ->where('tercero_id', $padre_tres->id)
                                                         ->where('nivel', 3)
                                                         ->first();
+
                                                     if (count($result) > 0) {
                                                         DB::table('terceros_niveles')
                                                             ->where('tercero_id', $padre_tres->id)
                                                             ->where('nivel', 3)
                                                             ->update(['puntos' => $result->puntos + $puntos]);
+
                                                     } else {
                                                         DB::table('terceros_niveles')->insertGetId(
                                                             [
@@ -251,7 +280,6 @@ trait OrderPaid
                         }
                     }
                 }
-                
             }
             $update = Order::find($order_create->id);
             $update->cargue_puntos = Carbon::now();
