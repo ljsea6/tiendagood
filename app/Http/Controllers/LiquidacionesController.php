4d2d24c97d9cc2d54b67b306b4473ae5c7497d50
= <=>
2017-12-20 16:57:22 -0500

    cambio liquidacion

--- app/Http/Controllers/LiquidacionesController.php
+++ app/Http/Controllers/LiquidacionesController.php
@@ -49,7 +49,7 @@ class LiquidacionesController extends Controller {
         $vendedores_liquidados = array(); 
 
 
-        $vendedores = DB::table('terceros as t')->where('t.tipo_cliente_id', 83)->select('t.id', 't.tipo_id')->limit(100)->orderByRaw('id ASC')->get();
+        $vendedores = DB::table('terceros as t')->where('t.tipo_cliente_id', 83)->select('t.id', 't.tipo_id')->orderByRaw('id ASC')->get();
                     
         foreach ($vendedores as $value_vendedor) { 
 
