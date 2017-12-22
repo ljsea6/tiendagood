71570545628023709e73080d8792e661d180f416
diegobellodev <desarrollo02@tiendagood.com>
2017-12-22 16:12:42 -0500

    cambio en el dashboard para ver mis puntos de los amparados ,mas las nuevas condiciones 2

--- app/Console/Commands/SetTransactions.php
+++ app/Console/Commands/SetTransactions.php
@@ -48,53 +48,52 @@ class SetTransactions extends Command {
         $noguardadas = array();
 
         $client = new \GuzzleHttp\Client();
-         $this->info('hola');    
+        $this->info('hola');
         $ordenes = Order::select('order_id', 'shop')
                 ->where("financial_status", "paid")
                 ->get();
-        $this->info('hola');
-        /*
-          foreach ($ordenes as $orden) {
-          if ($orden->shop != NULL) {
 
-          if ($orden->shop == 'good') {
-          $api = 'https://' . env('API_KEY_SHOPIFY') . ':' . env('API_PASSWORD_SHOPIFY') . '@' . env('API_SHOP');
-          } elseif ($orden->shop == 'mercando') {
-          $api = 'https://' . env('API_KEY_MERCANDO') . ':' . env('API_PASSWORD_MERCANDO') . '@' . env('API_SHOP_MERCANDO');
-          }
+        foreach ($ordenes as $orden) {
+            if ($orden->shop != NULL) {
 
-          try {
+                if ($orden->shop == 'good') {
+                    $api = 'https://' . env('API_KEY_SHOPIFY') . ':' . env('API_PASSWORD_SHOPIFY') . '@' . env('API_SHOP');
+                } elseif ($orden->shop == 'mercando') {
+                    $api = 'https://' . env('API_KEY_MERCANDO') . ':' . env('API_PASSWORD_MERCANDO') . '@' . env('API_SHOP_MERCANDO');
+                }
 
-          $shop = $client->request('GET', $api . "/admin/orders/$orden->order_id/transactions.json");
-          $headers = $shop->getHeaders()['X-Shopify-Shop-Api-Call-Limit'];
-          $x = explode('/', $headers[0]);
-          $diferencia = $x[1] - $x[0];
-          if ($diferencia < 20) {
-          usleep(20000000);
-          }
+                try {
 
-          $results = json_decode($shop->getBody(), true);
-          if (count($results['transactions']) > 0) {
-          foreach ($results['transactions'] as $transaction) {
-          $save = Transactions::saveTransaction($transaction, $orden->shop);
-          if ($save) {
-          $totalguardadas++;
-          } else {
-          $noguardadas[] = $transaction['id'];
-          }
-          }
-          } else {
-          $this->info('No se encuentran transacciones por la orden con id ' + $orden->order_id);
-          }
-          } catch (ClientException $e) {
+                    $shop = $client->request('GET', $api . "/admin/orders/$orden->order_id/transactions.json");
+                    $headers = $shop->getHeaders()['X-Shopify-Shop-Api-Call-Limit'];
+                    $x = explode('/', $headers[0]);
+                    $diferencia = $x[1] - $x[0];
+                    if ($diferencia < 20) {
+                        usleep(20000000);
+                    }
 
-          if ($e->hasResponse()) {
-          return redirect()->back()->with(['err' => ' No se encuentra la order' + $orden->order_id]);
-          }
-          }
-          }
-          }
-         */
+                    $results = json_decode($shop->getBody(), true);
+                    if (count($results['transactions']) > 0) {
+                        foreach ($results['transactions'] as $transaction) {
+                            $save = Transactions::saveTransaction($transaction, $orden->shop);
+                            if ($save) {
+                                $totalguardadas++;
+                            } else {
+                                $noguardadas[] = $transaction['id'];
+                            }
+                        }
+                    } else {
+                        $this->info('No se encuentran transacciones por la orden con id ' + $orden->order_id);
+                    }
+                } catch (ClientException $e) {
+
+                    if ($e->hasResponse()) {
+                        return redirect()->back()->with(['err' => ' No se encuentra la order' + $orden->order_id]);
+                    }
+                }
+            }
+        }
+
         $this->info("Se guardaron $totalguardadas transacciones en total");
         if (count($noguardadas) > 0) {
             $this->info(count($noguardadas) . " transacciones no se guardaron:");
@@ -102,8 +101,6 @@ class SetTransactions extends Command {
         } else {
             $this->info('Proceso se ha realizado con exito');
         }
-        
-           $this->info('holav');
     }
 
 }
